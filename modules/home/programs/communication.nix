f5cee049e3af423b16f43f132a7609ac20d2a86f
Glomzzz <glom@skillw.com>
2026-05-02 00:23:28 +0800

    add(cherry-studio)

--- modules/home/programs/communication.nix
+++ modules/home/programs/communication.nix
@@ -7,5 +7,17 @@
     mumble
     discord
     anydesk
+    cherry-studio
   ];
+
+  # Add Cherry Studio to application menu
+  xdg.desktopEntries.cherry-studio = {
+    name = "Cherry Studio";
+    genericName = "AI Chat Client";
+    exec = "cherry-studio %U";
+    icon = "cherry-studio";
+    categories = [ "Network" "Chat" "Qt" ];
+    mimeType = [ "x-scheme-handler/cherrystudio" ];
+    comment = "Desktop client for LLMs and AI tools";
+  };
 }
---
+++ pkgs/cherry-studio.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  fetchurl,
+  appimageTools,
+}:
+let
+  pname = "cherry-studio";
+  version = "1.9.4";
+  src = fetchurl {
+    url = "https://github.com/CherryHQ/cherry-studio/releases/download/v${version}/Cherry-Studio-${version}-x86_64.AppImage";
+    hash = "sha256-79sccZCTuhahtMP9OIEI165Ce/ZppUxzf5Q/Vmz6isc=";
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraPkgs = pkgs: with pkgs; [
+    libsecret
+    libnotify
+  ];
+
+  meta = {
+    description = "Desktop client for LLMs and AI tools";
+    homepage = "https://github.com/CherryHQ/cherry-studio";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "cherry-studio";
+    platforms = [ "x86_64-linux" ];
+    maintainers = [ ];
+  };
+}
